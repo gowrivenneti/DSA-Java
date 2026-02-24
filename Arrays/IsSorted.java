571c67c887bae06819a1b7d801936b7cb1ec7b2d
gowrivenneti <lakshmigowrivenneti@gmail.com>
2026-02-24 19:16:57 +0530

    Implement IsSorted class to check array order

---
+++ Arrays/IsSorted.java
@@ -0,0 +1,13 @@
+class IsSorted {
+    public boolean isSorted(int[] arr) {
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>=arr[i-1]){
+                
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
