d501930d611f0d21490fa27830511f7b070222fc
gowrivenneti <lakshmigowrivenneti@gmail.com>
2026-02-24 19:26:02 +0530

    Remove Duplicates and return the unique elements count
    Returns the unique elements count after removing duplicates.

---
+++ Arrays/RemoveDuplicates.java
@@ -0,0 +1,12 @@
+class RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+        int i=0;
+        for(int j=1;j<nums.length;j++){
+            if(nums[i]!=nums[j]){
+                nums[i+1]=nums[j];
+                i++;
+            }
+        }
+        return i+1;  // returns the unique elements count, after removing duplicates
+    }
+}
